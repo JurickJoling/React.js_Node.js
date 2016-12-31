2952b79b17ec17cc7d9725e6f5f8ddf8cf814dac
Vitaliy Polotskiy <vpolotskiy@gmail.com>
2016-12-31 06:52:19 +0300

    Fixes

--- src/helpers/renderMultiselect.js
+++ src/helpers/renderMultiselect.js
@@ -6,10 +6,13 @@ function renderMultiselect({ input, label, meta: { touched, error, warning }, ..
   return (
     <fieldset className={cl('form-group', { 'has-error': (touched && error) })}>
       {label ? <label>{label}</label> : null}
-      <Multiselect {...input}
-                   onBlur={() => input.onBlur()}
-                   value={input.value || []}
-                   {...rest} />
+      <Multiselect
+        {...input}
+        onBlur={() => input.onBlur()}
+        value={input.value || []}
+        {...rest}
+        filter="contains"
+      />
       {touched && ((error && <div className="error help-block">{error}</div>) || (warning && <div className="error">{warning}</div>))}
     </fieldset>
   );
