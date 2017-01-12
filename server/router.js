f8951b84d77edcb35b4a60bf63f12af15e92aa94
Vitaliy Polotskiy <vpolotskiy@gmail.com>
2017-01-13 00:43:44 +0300

    Added eventbrite controller

--- server/router.js
+++ server/router.js
@@ -1,10 +1,14 @@
 const dataController = require('./controllers/data');
 const uploadController = require('./controllers/upload');
+const eventbriteController = require('./controllers/eventbrite');
 const yelpController = require('./controllers/yelp');
 
 module.exports = app => {
   app.get('/data', dataController);
   app.post('/upload', uploadController);
+  app.post('/eventbrite/search', eventbriteController.search);
+  app.get('/eventbrite/organizers/:id', eventbriteController.organizers);
+  app.get('/eventbrite/events/:id/tickets', eventbriteController.tickets);
   app.post('/yelp', yelpController.index);
   app.get('/yelp/:id', yelpController.show);
 };
--- src/helpers/EventbriteFinder.js
+++ src/helpers/EventbriteFinder.js
@@ -1,11 +1,11 @@
 import isObject from 'lodash/isObject';
 import size from 'lodash/size';
 import React, { Component, PropTypes } from 'react';
-import { Button, Modal } from 'react-bootstrap';
+import { Button, Modal, Popover, OverlayTrigger } from 'react-bootstrap';
 import cl from 'classnames';
 import axios from 'axios';
 
-import { EVENTBRITE_API_HOST, EVENTBRITE_TOKEN } from '../config';
+import { EVENTBRITE_API_HOST, EVENTBRITE_TOKEN, YELP_HOST_URI } from '../config';
 
 export default class EventbriteFinder extends Component {
 
@@ -19,6 +19,8 @@ export default class EventbriteFinder extends Component {
   state = {
     modalOpened: false,
     q: '',
+    organizers: {},
+    tickets: {},
     events: [],
     errorMessage: null
   };
@@ -37,28 +39,67 @@ export default class EventbriteFinder extends Component {
 
   search() {
     const { q } = this.state;
-    axios.get(`${EVENTBRITE_API_HOST}?q=${q}&token=${EVENTBRITE_TOKEN}`)
+
+    // const url = `${EVENTBRITE_API_HOST}/events/search/?q=${q}&token=${EVENTBRITE_TOKEN}`;
+
+    axios.post(`http://localhost:3000/eventbrite/search`, { q })
       .then(({ data: { events } }) => this.setState({ events }))
       .catch(({ errorMessage }) => this.setState({ errorMessage }));
   }
 
-  onSelect(event) {
+  onSelect(event, ticket) {
     const { input } = this.props;
 
     console.log('event', event);
 
     this.toggleModal(() => input.onChange({
-      id: event.id,
-      name: event.name,
-      description: event.description,
-      organizer_id: event.organizer_id,
-      url: event.url
+      event: {
+        id: event.id,
+        name: event.name,
+        description: event.description,
+        organizer_id: event.organizer_id,
+        url: event.url
+      },
+      tickets: [
+        ...(input.value.tickets || []),
+        ticket
+      ]
     }));
+  }
+
+  unSelect() {
+
+  }
+
+  showOrganizer(organizerId) {
+    const { organizers } = this.state;
+
+    axios.get(`http://localhost:3000/eventbrite/organizers/${organizerId}`)
+      .then(({ data }) => this.setState({
+        organizers: {
+          ...organizers,
+          [organizerId]: data
+        }
+      }))
+      .catch(({ errorMessage }) => this.setState({ errorMessage }));
+  }
+
+  showTickets(eventId) {
+    const { tickets } = this.state;
+
+    axios.get(`http://localhost:3000/eventbrite/events/${eventId}/tickets`)
+      .then(({ data: { ticket_classes } }) => this.setState({
+        tickets: {
+          ...tickets,
+          [eventId]: ticket_classes
+        }
+      }))
+      .catch(({ errorMessage }) => this.setState({ errorMessage }));
   }
 
   render() {
     const { input, label, placeholder, meta: { touched, error, warning } } = this.props;
-    const { q, events, showModal } = this.state;
+    const { q, events, organizers, tickets, showModal } = this.state;
 
     return (
       <fieldset className={cl('form-group', { 'has-error': (touched && error) })}>
@@ -79,11 +120,41 @@ export default class EventbriteFinder extends Component {
           <table className="table table-bordered table-striped table-hover m-t">
             <tbody>
             <tr>
-              <td>{input.value.name ? input.value.name.text : null}</td>
-              <td>{input.value.description ? input.value.description.text : null}</td>
-              <td>{input.value.organizer_id ? input.value.organizer_id : null}</td>
+              <td>{input.value.event.name ? input.value.event.name.text : null}</td>
               <td>
-                {input.value.url ? <a className="btn btn-primary" target="_blank" href={input.value.url}>More Info</a> : null}
+                {input.value.event.description ? (
+                    <OverlayTrigger
+                      trigger={['hover', 'focus']}
+                      placement="left"
+                      overlay={(
+                        <Popover id={`event-${event.id}-description`} title="Description">
+                          {input.value.event.description.text}
+                        </Popover>
+                      )}
+                    >
+                      <div>{(input.value.event.description.text || '').slice(0, 100)}</div>
+                    </OverlayTrigger>
+                  ) : null}
+              </td>
+              <td>
+                {organizers[input.value.event.organizer_id] ? organizers[input.value.event.organizer_id].name : (
+                    <Button className="btn btn-default" onClick={() => this.showOrganizer(input.value.event.organizer_id)}>Show Organizer</Button>
+                  )}
+              </td>
+              <td>
+                <table className="table table-hover table-bordered table-striped">
+                  <tbody>
+                  {input.value.tickets.map(ticket => (
+                    <tr key={ticket.id}>
+                      <td>{ticket.name}</td>
+                      <td>{ticket.cost ? ticket.cost.display : null}</td>
+                    </tr>
+                  ))}
+                  </tbody>
+                </table>
+              </td>
+              <td>
+                {input.value.event.url ? <a className="btn btn-primary" target="_blank" href={input.value.event.url}>More Info</a> : null}
               </td>
             </tr>
             </tbody>
@@ -133,13 +204,47 @@ export default class EventbriteFinder extends Component {
                       {events.map(event => (
                         <tr key={event.id}>
                           <td>{event.name ? event.name.text : null}</td>
-                          <td>{event.description ? event.description.text : null}</td>
-                          <td>{event.organizer_id}</td>
                           <td>
-                            <a className="btn btn-primary" target="_blank" href={event.url}>More Info</a>
+                            {event.description ? (
+                                <OverlayTrigger
+                                  trigger={['hover', 'focus']}
+                                  placement="left"
+                                  overlay={(
+                                    <Popover id={`event-${event.id}-description`} title="Description">
+                                      {event.description.text}
+                                    </Popover>
+                                  )}
+                                >
+                                  <div>{(event.description.text || '').slice(0, 150)}</div>
+                                </OverlayTrigger>
+                              ) : null}
                           </td>
                           <td>
-                            <Button bsStyle="success" onClick={() => this.onSelect(event)}>Select</Button>
+                            {organizers[event.organizer_id] ? organizers[event.organizer_id].name : (
+                                <Button className="btn btn-default" onClick={() => this.showOrganizer(event.organizer_id)}>Show Organizer</Button>
+                              )}
+                          </td>
+                          <td>
+                            {tickets[event.id] ? (
+                              <table className="table table-hover table-bordered table-striped">
+                                <tbody>
+                                {tickets[event.id].map(ticket => (
+                                  <tr key={ticket.id}>
+                                    <td>{ticket.name}</td>
+                                    <td>{ticket.cost ? ticket.cost.display : null}</td>
+                                    <td>
+                                      <Button bsStyle="success" onClick={() => this.onSelect(event, ticket)}>Select</Button>
+                                    </td>
+                                  </tr>
+                                ))}
+                                </tbody>
+                              </table>
+                              ) : (
+                                <Button className="btn btn-default" onClick={() => this.showTickets(event.id)}>Show Tickets</Button>
+                              )}
+                          </td>
+                          <td>
+                            <a className="btn btn-primary" target="_blank" href={event.url}>More Info</a>
                           </td>
                         </tr>
                       ))}
